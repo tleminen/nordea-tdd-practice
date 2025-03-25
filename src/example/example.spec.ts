8d3d4e9616c72d2c60b405c91dac7c0053bb20ba
Tomi Leminen <tleminen@student.uef.fi>
2025-03-25 10:52:33 +0200

    returns array with one element when one name is given

--- src/example/example.spec.ts
+++ src/example/example.spec.ts
@@ -13,10 +13,17 @@ Sort array of String
 describe('Sort array of Strings', () => {
     it('returs empty array when no names are given', () => {
         const result = sortArray();
+
         expect(result).toEqual([]);
     });
+
+    it('returns array with one element when one name is given', () => {
+        const result = sortArray("Tomi");
+
+        expect(result).toEqual(["Tomi"]);
+    })
 });
 
-function sortArray(): string[] {
-    return [];
+function sortArray(...name: string[]): string[] {
+    return name;
 }
